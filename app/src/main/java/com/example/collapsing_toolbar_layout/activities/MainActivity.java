e090f1b65fe2d26870729550ace96a9dcd22f15f
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-25 13:40:09 +0530

    Refactored teh the Theme.

--- app/src/main/java/com/example/collapsing_toolbar_layout/activities/MainActivity.java
+++ app/src/main/java/com/example/collapsing_toolbar_layout/activities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.collapsing_toolbar_layout.R;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         // Toolbar should be androidx version.
         Toolbar toolbar = findViewById(R.id.tool_bar);
